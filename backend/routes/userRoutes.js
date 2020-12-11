2cff5a3bf220b2c1f4cb0a62325bccfe1e486acb
chienvu94 <chienvv@massgenie.com>
2020-12-11 10:36:19 +0700

    7. Login user backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const userModel = require("../models/userModel");
 const User = require('../models/userModel')
 
 router.post("/register", async (req, res) => {
@@ -38,5 +40,50 @@ router.post("/register", async (req, res) => {
       res.status(500).json({ error })
   }
 });
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({
+                message: "Please enter email or password"
+            })
+        }
+
+        if(password.length < 5) {
+            return res.status(400).json({
+                message: "Please enter password at least 5 character"
+            })
+        }
+
+        const existingUser = await User.findOne({ email: email });
+        if(existingUser) {
+            const isMatchPassword = await bcryptjs.compare(password, existingUser.password);
+            
+            if(!isMatchPassword) {
+                return res.status(400).json({
+                    message: "Invalid password"
+                })
+            }
+            
+            const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+            
+            res.status(200).json({
+                token,
+                email: existingUser.email,
+                id: existingUser._id
+            })
+
+        } else {
+            return res.status(400).json({
+                message: "No user found"
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({error})
+    }
+})
 
 module.exports = router;
